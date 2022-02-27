e32d365e27c0bc67ae72088313caa3230ac496cf
Sultan Parbes <sultanp@gmail.com>
2022-02-27 23:59:23 +0000

    added input validation

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
+const { body, validationResult } = require('express-validator');
 
 exports.getIndex = function (req, res, next) {
   res.render('index', {
@@ -11,22 +12,33 @@ exports.getIndex = function (req, res, next) {
 };
 
 exports.getSignUp = function (req, res, next) {
-  res.render('sign_up', { title: 'Sign Up' });
+  res.render('sign_up', { title: 'Sign Up', errors: null });
 };
 
-exports.postSignUp = function (req, res, next) {
-  bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
-    const user = new User({
-      username: req.body.username,
-      password: hashedPassword,
-    }).save((err) => {
-      if (err) {
-        return next(err);
-      }
-      res.redirect('/');
+exports.postSignUp = [
+  body('username', 'Invalid username').escape().trim(),
+  body('password', 'Invalid username').trim().isLength({ min: 6 }),
+  function (req, res, next) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render('sign_up', { title: 'Sign Up', errors: errors.array() });
+    }
+    User.find({ username: req.body.username });
+
+    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+      const user = new User({
+        username: req.body.username,
+        password: hashedPassword,
+      }).save((err) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect('/');
+      });
     });
-  });
-};
+  },
+];
 
 exports.getLogIn = function (req, res, next) {
   res.render('log_in', {
